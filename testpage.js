0e845585ea93ea02a5a5964f0bc7557fa0061949
czhanacek <Charlie@Kitchen-iMac.local>
2015-05-18 19:38:46 -0700

    Working Code
    I have set 1 of A-B loading in for testing

--- testpage.js
+++ testpage.js
@@ -2,7 +2,7 @@ var wordsCorrect = 0;
 var currentWordIndex = 0;
 var wordList = []
 var userResponseList = []
-
+var responseImg;
 function readCookie(name) {
 	var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
 	result && (result = JSON.parse(result[1]));
@@ -13,7 +13,7 @@ function loadTest() {
 	//var testObject = readCookie(testObject);
 	// Eventually this will read a value from the cookie.
 	// For now, it will just instantiate the wordList array
-	wordList = ["cat", "dog", "mouse", "fish"];
+	wordList = ["the", "of", "and", "a", "to"];
 	
 	console.log(wordList[currentWordIndex]);
 	doWord(wordList[currentWordIndex]);
@@ -28,20 +28,28 @@ function doWord(word) {
 }
 function wordSubmitted() {
 	var userSubmission = document.getElementById("wordSubmission").value;
+	document.getElementById("wordSubmission").value = "";
 	userResponseList.push(userSubmission);
-	if(userSubmission === wordList[currentWordIndex]) {
+	if(userSubmission.toLowerCase() === wordList[currentWordIndex].toLowerCase()) {
 		wordsCorrect++;
 		console.log(wordsCorrect);
-		playCorrect();
+		doCorrect();
 	}
 	else {
-		playWrong();
+		doWrong();
 	}
+	
 	if(wordList.length - 1 > currentWordIndex) {
 		currentWordIndex++;
 	}
-	doWord(wordList[currentWordIndex]);
+	else {
+		currentWordIndex = 0;
+		
+		//proceedToResults();
+	}
 	
+	doWord(wordList[currentWordIndex]);
+	//document.getElementById("feedbackImg").src = "";
 }
 function bake_cookie(name, value) {
 	var cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
@@ -52,18 +60,29 @@ function bake_cookie(name, value) {
 function playWordAudio(word) {
 	playAudioFile(word + ".mp3");
 }
-function playCorrect() {
+function doCorrect() {
 	playAudioFile("crct.wav");
+	responseImage("green-check.png");
 }
-function playWrong() {
+function doWrong() {
 	playAudioFile("wrg.wav");
+	responseImage("red-x.png");
+}
+function responseImage(img) {
+	var text = "<img style=\"height: 25%; width: 25%; display: block;margin-left: auto; margin-right: auto;\" class=\"img-responsive\" src=\"img/" + img + "\"></img>"
+	document.getElementById("imageContainer").innerHTML = text;
+	setTimeout(removeImage, 1000);
+}
+function removeImage() {
+	document.getElementById("imageContainer").innerHTML = "";
+	
 }
 function playAudioFile(file) {
 	var soundFile = document.createElement("audio");
 	soundFile.preload = "auto";
 	soundFile.src = "audioFiles/" + file;
 	soundFile.load();
-	soundFile.currentTime = 0.01;
+	soundFile.currentTime = -1.00;
 	soundFile.play();
 	soundFile.remove();
 }
